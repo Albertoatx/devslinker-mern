01e9b75edd44688774fe84d1dd66c70156512c1c
amm <f1smydrug@gmail.com>
2019-02-08 11:44:00 +0100

    Add 2 public routes: list all posts, get a post. Add 2 protected routes: delete, update a post

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -5,6 +5,47 @@ const Profile = require('../models/Profile');
 // Load Input Validation functions
 const validatePostInput = require('../validation/post.validator');
 
+
+// ----------------------------------------------------------------------------
+// getAllPosts - A controller method that gets all the posts 
+// ----------------------------------------------------------------------------
+exports.getAllPosts = (req, res) => {
+
+  // const errors = {}; don't need it because maximum 1 error (no validations)
+
+  Post.find() // returns an empty array if no matches
+    .sort({ date: -1 })
+    .then(posts => {
+      if (!posts || posts.length === 0) {
+        return res.status(404).json({ noposts: 'There are no posts' });
+      }
+
+      res.json(posts);
+    }
+    )
+    .catch(err => res.status(400).json(err));
+  //.catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
+}
+
+// ----------------------------------------------------------------------------
+// getPostById - A controller method that gets the post information 
+//               using the 'post_id' request param 
+// ----------------------------------------------------------------------------
+exports.getPostById = (req, res) => {
+
+  Post.findById(req.params.post_id)
+    .then(post => {
+
+      if (!post) {
+        return res.status(404).json({ noposts: 'There is no post with that ID' });
+      }
+
+      res.json(post);
+    })
+    .catch(err => res.status(400).json(err));
+  //.catch(err => res.status(404).json({ nopostfound: 'No posts found' }));
+}
+
 // ----------------------------------------------------------------------------
 // addPost - A controller method that adds a post 
 // ----------------------------------------------------------------------------
@@ -24,5 +65,105 @@ exports.addPost = (req, res) => {
     user: req.user.id
   });
 
-  newPost.save().then(post => res.json(post));
+  newPost.save()
+    .then(post => res.json(post))
+    .catch(err => res.status(400).json(err));
+}
+
+// ----------------------------------------------------------------------------
+// updatePost - A controller method that updates a post using the 'post_id' 
+//              request param. Check that ONLY owner of the post can delete 
+// ----------------------------------------------------------------------------
+exports.updatePost = (req, res) => {
+
+  const { errors, isValid } = validatePostInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  // 1 DB access
+  Post.findOneAndUpdate(
+    {
+      _id: req.params.post_id,
+      user: req.user.id        // Check for post owner
+    },
+    {
+      $set: { text: req.body.text }
+    },
+    {
+      new: true,  // return the updated version
+    }
+  ).then((post) => {
+
+    if (!post) {
+      return res.status(404).json({
+        error: 'Error updating post: Post not found or User not authorizated',
+      });
+    }
+
+    res.json(post);
+  }).catch((err) => {
+    res.status(400).json(err);
+    //res.status(500).json({ 'error': 'error updating post' });
+  })
+
+  /* 2 DB accesses but better error messaging (it detects when user is not owner)
+  Post.findOne({ _id: req.params.post_id })
+    .then(post => {
+      //check post owner
+      if (post.user.toString() !== req.user.id) {
+        return res.status(401).json({ notauthorized: 'User not authorizated' })
+      }
+      post.text = req.body.text
+
+      post.save().then(post => res.json(post))
+    })
+    .catch(err => res.status(404).json({ postnotfound: 'No post found' }))
+   */
+
+}
+
+
+// ----------------------------------------------------------------------------
+// deletePost - A controller method that deletes a post using the 'post_id' 
+//              request param. Check that ONLY owner of the post can delete 
+// ----------------------------------------------------------------------------
+exports.deletePost = (req, res) => {
+
+  Post.findOneAndDelete(
+    {
+      _id: req.params.post_id,
+      user: req.user.id        // Check for post owner
+    })
+    .then(post => {
+      if (!post) {//check to see if a post was found/deleted
+        res.status(404).json({ nopost: "no post found with that ID or user is not the post owner" });
+      }
+
+      res.json(post); // returns the deleted post
+    })
+    .catch(err => res.status(400).json(err)); //If _id is malformed it goes here
+  //.catch(err => res.status(400).json({ nopost: "there was a problem deleting post...", err }));
+
+
+  // Traversy version - No use doing a 'Profile.findOne' (read forum)
+  /*                  - It does 3 DB accesses
+  Profile.findOne({ user: req.user.id }).then(profile => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // Check for post owner
+        if (post.user.toString() !== req.user.id) {
+          return res
+            .status(401)
+            .json({ notauthorized: 'User not authorized' });
+        }
+
+        // Delete
+        post.remove().then(() => res.json({ success: true }));
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+  });
+  */
 }
--- routes/api/posts.route.js
+++ routes/api/posts.route.js
@@ -8,12 +8,42 @@ const postCtrl = require('../../controllers/posts.controller');
 router.get('/test', (req, res) => res.json({ msg: "Posts Works" }));
 
 //-----------------------------------------------------------------------------
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get('/', postCtrl.getAllPosts);
+
+//-----------------------------------------------------------------------------
+// @route   GET api/posts/:post_id
+// @desc    Get post by id
+// @access  Public
+router.get('/:post_id', postCtrl.getPostById);
+
+//-----------------------------------------------------------------------------
 // @route   POST api/posts
 // @desc    Create post
 // @access  Private
 router.post('/',
   passport.authenticate('jwt', { session: false }),
   postCtrl.addPost
+);
+
+//-----------------------------------------------------------------------------
+// @route   PUT api/posts/:post_id
+// @desc    Update post
+// @access  Private
+router.put('/:post_id',
+  passport.authenticate('jwt', { session: false }),
+  postCtrl.updatePost
+);
+
+//-----------------------------------------------------------------------------
+// @route   DELETE api/posts/:post_id
+// @desc    Delete post
+// @access  Private
+router.delete('/:post_id',
+  passport.authenticate('jwt', { session: false }),
+  postCtrl.deletePost
 );
 
 module.exports = router;
